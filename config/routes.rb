b5811d1b619c3950c55e37d5ab031af33a81b2fc
Junichi Fujita <fujita0909j@gmail.com>
2020-02-08 11:30:13 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'blogs/index'
+  get 'blogs/new'
+  get 'blogs/show'
+  get 'blogs/edit'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
